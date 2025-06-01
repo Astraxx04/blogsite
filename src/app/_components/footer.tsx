0ae3ac31fd31bc66f4f5936120809399003b7507
Astraxx04 <gagan200254@gmail.com>
2025-06-01 19:00:30 +0530

    Minor footer changes

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,14 +2,14 @@ import Container from '@/app/_components/container';
 
 export function Footer() {
     return (
-        <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
+        <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-black">
             <Container>
                 <div className="flex flex-col lg:flex-row items-center lg:mx-32 md:mx-32">
                     <h4 className="text-3xl pt-4 lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-6 lg:mb-0 lg:pr-4 lg:w-1/2">
                         Get In Touch.
                     </h4>
                     <div className="flex flex-col lg:flex-row lg:justify-end justify-center lg:items-end lg:pl-4 lg:w-1/2 w-2/3">
-                        <div className="social-icon flex justify-between lg:pt-5 md:pt-0 mr-0 pb-4">
+                        <div className="social-icon flex justify-between lg:pt-5 md:pt-0 mr-0 pb-4 gap-x-6">
                             <div className="lg:size-16 md:size-16 size-12">
                                 <a
                                     href="https://github.com/Astraxx04"
